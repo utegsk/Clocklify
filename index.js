85334da5e3e3fce67b43925b2171608201b7fcaf
jakubzzak <zak.jakub67@gmail.com>
2021-02-22 21:05:35 +0100

    extreme refactoring #2

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
-import { 
-  startWork,
-  stopWork,
-  toggleBreak,
-  deleteWork,
-  workStatus,
-  help,
-  importEntries,
-  logWork,
-  printVersion,
-  unknownArgument
-} from './lib/core';
-
-var argv = require('minimist')(process.argv.slice(2));
-var args = argv._
+require('dotenv').config()
+const core = require('./lib/core.js');
+const argv = require('minimist')(process.argv.slice(2));
+const args = argv._;
 
 
 const main = async () => {
@@ -21,42 +10,44 @@ const main = async () => {
     switch(args[0]) {
       case 'start':
       case 'in'   :
-        startWork(argv)
+        core.startWork(argv)
         break
       case 'stop':
       case 'out' :
       case 'quit':
       case 'exit':
       case 'end' :
-        stopWork()    
+      case 'done':
+        core.stopWork()
         break
       case 'break':
       case 'pause':
       case 'lunch':
-        toggleBreak()
+        core.toggleBreak()
         break
       case 'remove':
-      case 'throw':
-      case 'dump':
-        deleteWork()
+      case 'throw' :
+      case 'clear' :
+      case 'dump'  :
+        core.deleteWork()
         break
       case 'status':
-        workStatus()
+        core.workStatus()
         break
       case 'help':
-        help()
+        core.help()
         break
       case 'log':
-        logWork(args[1], args[2])
+        core.logWork(args[1], args[2])
         break
       case 'import':
-        importEntries(args[1])
+        core.importEntries(args[1])
         break
       case 'version':
-        printVersion()
+        core.printVersion()
         break
       default:
-        unknownArgument(args[0])
+        core.unknownArgument(args[0])
     }
   } catch(error) {
     console.error(error.message)
